780ede738007be0796971bd965a766f99afbd0c9
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-14 14:46:01 -0700

    able to list data from firebase to html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,13 +9,33 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
-  
+   database.ref().on("child_added",function(childSnapshot){
+    
+      console.log("Name :"+childSnapshot.val().name);
+      console.log("Destination "+childSnapshot.val().destination);
+      console.log("Train Time :"+childSnapshot.val().trainTime);
+      console.log("Frequency "+childSnapshot.val().frequency);
+      var dateFormat=moment(childSnapshot.val().dateAdded).format("hh:mm a");
+      console.log("date format "+dateFormat);
+
+      
+      var tableRow=$("<tr>");  
+      var cell1=$("<td>").append(childSnapshot.val().name); 
+      var cell2=$("<td>").append(childSnapshot.val().destination); 
+      var cell3=$("<td>").append(childSnapshot.val().frequency);
+      var cell4= $("<td>").append(dateFormat);
+      var cell5= $("<td>").append("min");
+      tableRow.append(cell1).append(cell2).append(cell3).append(cell4).append(cell5);
+      $("tbody").append(tableRow);
+
+  }); 
+
 
     // Capture Button Click
     $("#add-train").on("click", function(event) {
       // prevent form from trying to submit/refresh the page
       event.preventDefault();
-      
+
       // Capture User Inputs and store them into variables
       var trainName = $("#tname-input").val().trim();
       var trainDestination = $("#destination-input").val().trim();
@@ -36,5 +56,10 @@ var config = {
     
   });
 
-     
+     /*  // Output all of the new information into the relevant HTML sections
+      $("#name-display").text(name);
+      $("#email-display").text(email);
+      $("#age-display").text(age);
+      $("#comment-display").text(comment); */
+
     });

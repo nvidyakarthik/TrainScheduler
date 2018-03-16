63382e4ae140189b3f378bc09a70ade7b9e86f46
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-16 12:25:58 -0700

    added signin page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
-var config = {
+  var config = {
     apiKey: "AIzaSyAc2wGtnh35TVciTvSu-FV8KWAYnEM-_iM",
     authDomain: "trainschedulerproject-b3734.firebaseapp.com",
     databaseURL: "https://trainschedulerproject-b3734.firebaseio.com",
     projectId: "trainschedulerproject-b3734",
-    storageBucket: "",
+    storageBucket: "trainschedulerproject-b3734.appspot.com",
     messagingSenderId: "216486738815"
   };
-  firebase.initializeApp(config);
-
+  firebase.initializeApp(config); 
   var database = firebase.database();
    database.ref().on("child_added",function(childSnapshot){
      
@@ -105,3 +104,26 @@ var config = {
       $("#comment-display").text(comment); */
 
     });
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    function signIn(){
+      alert("signin");
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log("This is a test user "+user);
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("This is a errow message "+errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    }

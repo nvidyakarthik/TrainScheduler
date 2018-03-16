8b8eff6094df9ec3169114dc6f3b5d6a40111c80
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-16 12:41:48 -0700

    added page redirection to trainschedule.html after login

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -107,19 +107,20 @@
 
     var provider = new firebase.auth.GoogleAuthProvider();
     function signIn(){
-      alert("signin");
+           
       firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
+        location.href="trainSchedule.html";
         console.log("This is a test user "+user);
         // ...
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log("This is a errow message "+errorMessage);
+        console.log("This is a error message "+errorMessage);
         // The email of the user's account used.
         var email = error.email;
         // The firebase.auth.AuthCredential type that was used.
